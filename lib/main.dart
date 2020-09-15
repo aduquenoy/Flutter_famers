ee9997c4f9a3c9a988fc03ec8c432a1e83aa6dd6
Arnaud Duquenoy <aduquenoy.shopping@gmail.com>
2020-09-15 16:02:00 +0200

    farmers_updatedPackages

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:farmers_market/src/app.dart';
-void main() => runApp(App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(App());
+}
